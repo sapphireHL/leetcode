2676079b9527f01cd48a17bf56b13e6d566ec41e
sapphireHL <sapphire57866@gmail.com>
2017-03-22 18:06:34 +0800

    easy

--- 026. Remove Duplicates from Sorted Array/Remove Duplicates from Sorted Array.cpp
+++ 026. Remove Duplicates from Sorted Array/Remove Duplicates from Sorted Array.cpp
@@ -4,7 +4,7 @@ public:
         if(nums.empty()) return 0;
         int count = 1;
         for(int i = 1; i < nums.size(); i++){
-            if(nums[i] != nums[i-1])
+            if(nums[i] != nums[count-1])
                 nums[count++] = nums[i];
         }
     	return count;
