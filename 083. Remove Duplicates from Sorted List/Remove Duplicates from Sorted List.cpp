4caa5957c145ebd4f7c6590f95d88142e4e83da5
sapphireHL <sapphire57866@gmail.com>
2017-04-12 22:29:46 +0800

    easy

--- 083. Remove Duplicates from Sorted List/Remove Duplicates from Sorted List.cpp
+++ 083. Remove Duplicates from Sorted List/Remove Duplicates from Sorted List.cpp
@@ -9,16 +9,12 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head == NULL) return NULL;
-        ListNode *current = head, *p = head, *result = head;
-        while(p != NULL){
-            if(p->val != current->val){
-                current->next = p;
-                current = current->next;
-            }
-            p = p->next;
+        ListNode *current = head, *p = head;
+        while(p && p->next){
+            if(p->val == p->next->val)
+                p->next = p->next->next;
+            else p = p->next;
         }
-        current->next = NULL;
-        return result;
+        return head;
     }
 };
