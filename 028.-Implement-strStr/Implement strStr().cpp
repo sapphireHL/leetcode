b222fb1dc61777cd3b5baf3116ccd96e9601e79d
sapphireHL <sapphire57866@gmail.com>
2017-04-27 10:42:02 +0800

    easy

--- 028.-Implement-strStr/Implement strStr().cpp
+++ 028.-Implement-strStr/Implement strStr().cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if(haystack.length() < needle.length()){
-            return -1;
+        int n = haystack.size()-needle.size();
+        for(int i=0; i<=n; i++){
+                int j=i, k=0;
+                while(k<needle.size() && j<haystack.size() && needle[k] == haystack[j]){
+                    j++;
+                    k++;
+                }
+                if(k == needle.size()) return i;
         }
-        int res = 0;
-        int isfind = 0;
-        for(int i=0; i<=haystack.length()-needle.length(); i++){
-            string str1 = haystack.substr(i, needle.length());
-            if(!strcmp(str1.c_str(), needle.c_str())){
-                res = i;
-                isfind = 1;
-                break;
-            }
-            else{
-             isfind = 0;   
-            }
-        }
-        return isfind?res:-1;
+        return -1;
+        
+        
+        // if(haystack.length() < needle.length()){
+        //     return -1;
+        // }
+        // int res = 0;
+        // int isfind = 0;
+        // for(int i=0; i<=haystack.length()-needle.length(); i++){
+        //     string str1 = haystack.substr(i, needle.length());
+        //     if(!strcmp(str1.c_str(), needle.c_str())){
+        //         res = i;
+        //         isfind = 1;
+        //         break;
+        //     }
+        //     else{
+        //      isfind = 0;   
+        //     }
+        // }
+        // return isfind?res:-1;
     }
 };
