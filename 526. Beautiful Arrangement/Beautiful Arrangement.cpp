fd837b85dee86fea27fc9328a921bb99a1b17631
sapphireHL <sapphire57866@gmail.com>
2017-05-30 12:39:19 +0800

    medium

--- 526. Beautiful Arrangement/Beautiful Arrangement.cpp
+++ 526. Beautiful Arrangement/Beautiful Arrangement.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    void dfs(int &res, vector<int>&p, int N, vector<int>&use){
-        if(p.size() == N) {
+    void dfs(int &res, int N, vector<int>&use, int pos){
+        if(pos > N) {
             res++;
             return;
         }
         for(int i=1; i<=N; i++){
-            if(!use[i] && (i%(p.size()+1) == 0 || (p.size()+1)%i == 0)){
-                p.push_back(i);
+            if(!use[i] && (i%pos == 0 || pos%i == 0)){
                 use[i] = 1;
-                dfs(res, p, N, use);
-                p.pop_back();
+                dfs(res, N, use, pos+1);
                 use[i] = 0;
             }
         }
     }
     int countArrangement(int N) {
-        int res = 0;
+        int res = 0, pos = 1;
         vector<int> use(N+1);
-        vector<int> p;
-        dfs(res, p, N, use);
+        dfs(res, N, use, pos);
         return res;
     }
 };
