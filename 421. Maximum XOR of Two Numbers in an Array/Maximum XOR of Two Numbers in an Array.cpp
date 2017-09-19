3d8e9e2e745c52a891f79e961f04a916f6b67a0f
sapphireHL <sapphire57866@gmail.com>
2017-09-19 11:07:55 +0800

    medium

--- 421. Maximum XOR of Two Numbers in an Array/Maximum XOR of Two Numbers in an Array.cpp
+++ 421. Maximum XOR of Two Numbers in an Array/Maximum XOR of Two Numbers in an Array.cpp
@@ -5,10 +5,11 @@ public:
         for(int i=31; i>=0; i--){
             unordered_set<int> set;
             mask |= (1<<i);
+            //计算出每个数字的最高i位
             for(int j=0; j<nums.size(); j++){
                 set.insert(nums[j] & mask);
             }
-            
+            //假设第i位为1， a^b=c -> a^c=b   b^c=a
             int prefix = res | (1<<i);
             for(auto i:set){
                 if(set.count(i ^ prefix)){
