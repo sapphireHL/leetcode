82b533dbafce7938c96d27121514b47f2a6c2572
sapphireHL <sapphire57866@gmail.com>
2017-09-21 17:04:23 +0800

    medium

---
+++ 398. Random Pick Index/Random Pick Index.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    unordered_map<int, vector<int>> mp;
+    vector<int> arr;
+    Solution(vector<int> nums) {
+        arr = nums;
+    }
+    //蓄水池抽样
+    int pick(int target) {
+        int count = 1, res = 0;
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i] == target){
+                if(rand() % count == 0)
+                    res = i;
+                count++;
+            }
+        }
+        return res;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
